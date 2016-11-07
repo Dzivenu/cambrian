d772ff63206a50384a34dad3f38deb0424010b0c
pharesim <pharesim@protonmail.com>
2016-11-07 20:30:35 +0100

    View data

--- modules/core/classes/View.php
+++ modules/core/classes/View.php
@@ -2,18 +2,21 @@
 Class CambrianView
 {
 
+  private $data = []
+
   public function parse() {
     $this->includeFile('mod_core_base');
     return true;
   }
 
-  public function includeFile($file) {
+  public function includeFile($file,$data = []) {
     $target = explode('_',$file);
     $filetypes = ['html'];
     foreach($filetypes as $type) {
-      $userfile = '../layout/'.$file.'.'.$type;
+      $userfile = '../layout/html/'.$file.'.'.$type;
       if($target[0] == 'mod') {
         $modulefile = '../modules/'.$target[1].'/layout/'.$type.'/'.$target[2].'.'.$type;
+        $this->data = $data;
         if(file_exists($userfile)) {
           include $userfile;
         } elseif(file_exists($modulefile)) {
