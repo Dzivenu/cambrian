2882368bd77acf5d392f059c19d6dc9fce9677eb
Alexander Drichel <alexander@drichel.com>
2016-11-09 19:43:47 +0100

    added content and layout to new branch

--- pages/docs/config.php
+++ pages/docs/config.php
@@ -6,6 +6,6 @@ $pageconfig = [
     'tags' => ''
   ],
   'bodyClass' => 'body',
-  'wrapperTemplate' => 'mod_core_wrapper',
+  'wrapperTemplate' => 'mod_core_wrapper_home',
   'pageTitle' => 'Cambrian CMS home'
 ];
