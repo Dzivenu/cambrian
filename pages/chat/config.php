5a5395f193ae96f6838b2a7b6647be4b4e8e38a7
pharesim <pharesim@protonmail.com>
2017-07-31 14:46:29 +0200

    config for chat page

---
+++ pages/chat/config.php
@@ -0,0 +1,11 @@
+<?php
+$pageconfig = [
+  'metadata' => [
+    'title' => 'PEvO Chat',
+    'description' => '',
+    'tags' => ''
+  ],
+  'bodyClass' => 'body',
+  'wrapperTemplate' => 'mod_core_wrapper_home',
+  'pageTitle' => 'Chat'
+];
